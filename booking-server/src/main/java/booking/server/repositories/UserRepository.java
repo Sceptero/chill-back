f9f872eca069415f05f38fcee0d89c87a6126070
pusty <pusty@DESKTOP-GEQBVVI>
2016-11-05 02:51:59 +0100

    comm

--- booking-server/src/main/java/booking/server/repositories/UserRepository.java
+++ booking-server/src/main/java/booking/server/repositories/UserRepository.java
@@ -1,12 +1,15 @@
 package booking.server.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import booking.server.entities.User;
+import booking.server.enums.UserLevel;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long>
 {
-	
+	List<User> findByUserLevel(UserLevel userLevel);
 }
--- booking-server/src/main/java/booking/server/services/user/UserServiceImpl.java
+++ booking-server/src/main/java/booking/server/services/user/UserServiceImpl.java
@@ -1,31 +1,45 @@
 package booking.server.services.user;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import booking.server.entities.User;
+import booking.server.enums.UserLevel;
 import booking.server.repositories.UserRepository;
 
+@Service
 public class UserServiceImpl
 	implements UserService{
 	
 	@Autowired
 	private UserRepository userRepository; 
 	
+	@Override
 	public Iterable<User> getUsers()
 	{
 		return userRepository.findAll();
 	}
 
+	@Override
 	public User getUserById(long id) 
 	{
 		return userRepository.findOne(id);
 	}
+	
+	@Override
+	public List<User> getUserByLevel(UserLevel userLevel) {
+		return userRepository.findByUserLevel(userLevel);
+	}
 
+	@Override
 	public void deleteUserById(long id) 
 	{
 		userRepository.delete(id);
 	}
 
+	@Override
 	public void saveUser(User user) {
 		userRepository.save(user);
 	}
